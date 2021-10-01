18847cfa4df923c76028a5d7bd64dd90c06e603d
Eider22 <eidercardona22@gmail.com>
2021-10-01 14:52:48 -0500

    instalación de rect-router-dom

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route path='/ventas'>
+            
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
