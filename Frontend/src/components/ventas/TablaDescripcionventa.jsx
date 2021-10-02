a6dd65713bb720352e43593faf4b6b071d93c145
Eider22 <eidercardona22@gmail.com>
2021-10-02 01:27:26 -0500

    creando pag de descripcion venta

---
+++ Frontend/src/components/ventas/TablaDescripcionventa.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TablaDescripcionventa = () => {
+    return (
+        <div>
+            <table class="table">
+                <thead class="table-dark">
+                    ...
+                </thead>
+                <tbody>
+                    ...
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TablaDescripcionventa
---
+++ Frontend/src/styles/pages/ventas/ventanaDescrpcionVenta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ventanaDescrpcionVenta = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ventanaDescrpcionVenta
