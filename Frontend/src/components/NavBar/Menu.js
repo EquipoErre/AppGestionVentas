81ff5360afe61b48e32dcddcf8e09f7f4aba6727
ltisoy <danieltisoy2001@gmail.com>
2021-10-02 20:31:31 -0500

    mod: Comentarios, ClassName de los Link
    - para llamar a este componente se necesita un useState
    - Se añadieron comentarios
    - la funcion nombraModulos() se modifica:
      - el className de los links se cambia dependiendo de la pagina actual
      - onclick permite actualizar la pagina actual, recibe una funcion

--- Frontend/src/components/NavBar/Menu.js
+++ Frontend/src/components/NavBar/Menu.js
@@ -1,34 +1,38 @@
 import { Link } from "react-router-dom";
+
+// este componente se usa en los layouts
+
 // modulosYrutas es una lista de objetos
-export default function Menu(props) {
- 
-    let paths=props.ModulosYRutas
-  
+export default function Menu({ ModulosYRutas, paginaActual, onChange }) {
+  // modulos y es una lista de objetos con informacion para cargar
+  // se encuentra en la carpeta la info que se carga está en la carpeta json
+
   const nombraModulos = () => {
     const resultado = [];
-
-    paths.forEach((modulo) => {
+    // los enlaces del menu se cargan a traves de una iteracion
+    
+    ModulosYRutas.forEach((modulo) => {
       // className={props.pagina === i ? "active" : ""}
       resultado.push(
-        
         <li key={modulo.id}>
-          <Link to={modulo.ruta} className='ruta'>{modulo.nombre}</Link>
+          <Link to={modulo.ruta} className={paginaActual === modulo.ruta ? "active" : "ruta"} onClick= {() => onChange(modulo.ruta)}>
+            {modulo.nombre}
+          </Link>
         </li>
       );
-    });
+    }
+    
+    );
     return resultado;
   };
 
   return (
     <nav className="lista-enlaces">
       <Link to="/">
-      <h1 className="titulo">Gestion de Ventas</h1>
+        <h1 className="titulo">Gestion de Ventas</h1>
       </Link>
 
-      <ul className="menu">
-        {nombraModulos()}
-      </ul>
-
+      <ul className="menu">{nombraModulos()}</ul>
     </nav>
   );
 }
