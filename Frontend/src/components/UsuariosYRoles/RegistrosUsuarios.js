dfffaa38439557c2027db4e4ca847226bfcbfae1
ltisoy <danieltisoy2001@gmail.com>
2021-10-07 17:29:13 -0500

    se agregan el boton de opciones como un componente en cada registro

--- Frontend/src/components/UsuariosYRoles/RegistrosUsuarios.js
+++ Frontend/src/components/UsuariosYRoles/RegistrosUsuarios.js
@@ -9,33 +9,7 @@ export default function RegistrosUsuarios(props) {
       <td>{props.estado}</td>
       <td>{props.nombre}</td>
       <td>
-        <div className="dropdown text-center">
-          <button
-            className="btn btn-primary dropdown-toggle"
-            type="button"
-            id="dropdownMenu2"
-            data-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-          >
-            opciones
-          </button>
-          <div
-            className="dropdown-menu text-center p-2 rounded-lg"
-            aria-labelledby="dropdownMenu2"
-          >
-            <div className="pb-2">
-              {/* hay que tener en cuenta las commillas invertidas en este caso */}
-              <Link to={`/usuarios/${props.id}`} className="btn btn-success btn-block">
-                Editar
-              </Link>
-            </div>
-
-            <a href="#!" className="btn btn-danger btn-block">
-              Eliminar
-            </a>
-          </div>
-        </div>
+        <MenuOpcionesTabla rutaEnviar={`/usuarios/${props.id}`} nombreBoton='Editar'/>
       </td>
     </tr>
   );
