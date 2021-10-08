b250909fe036a0388ae069712d5108b44bad4774
ltisoy <danieltisoy2001@gmail.com>
2021-10-08 17:42:50 -0500

    se añade una clase a la tabla de productos
    - para que tenga un padding

--- Frontend/src/pages/productos/MaestroProductos.jsx
+++ Frontend/src/pages/productos/MaestroProductos.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const MaestroProductos = () => {
     return (
-    <div>
+    <div className='container'>
         <h2 className="titulo1">MAESTRO DE PRODUCTOS</h2>
         <main>
             <section className="buscador">
